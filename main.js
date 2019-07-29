8fdd3bd2dbb20965ea3c2c00bbc1f15a277aad02
Dylan <zimracytray1@yahoo.com>
2019-07-29 04:48:38 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+pizzaWebsites = [
+    "https://www.papajohns.com/", 
+    "https://www.pizzahut.com/", 
+    "https://www.dominos.com/en/", 
+    "https://pizzaranch.com/", 
+    "https://www.papamurphys.com/",
+    "https://pizzamananoka.com/",
+    "https://www.chanticlearpizza.com/",
+    "https://www.toppers.com/"
+];
+
+$("#pizzaTime").on("click", function(){
+    randNum = (Math.floor(Math.random() * pizzaWebsites.length));
+    window.location.href = pizzaWebsites[randNum];
+});
+
+//papa johns, pizza hut, dominos, pizza ranch, papa murphy's, pizza man, chanticlear, toppers pizzas
